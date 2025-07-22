07946bcdc7cb1f49db2ea65afc827f690f9764d4
zadumka <90329284+zadumka@users.noreply.github.com>
2025-07-22 15:20:48 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,18 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+import { QueryClient } from '@tanstack/react-query';
+
 import App from './components/App/App.tsx';
 import 'modern-normalize';
 import './global.css';
 
-const queryClient = new QueryClient();
+
 
 createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        
-        <QueryClientProvider queryClient={queryClient}>
-            <App />
-        </QueryClientProvider>
-    </StrictMode>,
+  <StrictMode>
+    {}
+    <App />
+  </StrictMode>,
 );
+
